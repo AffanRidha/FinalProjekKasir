663960811fceb660b9d05b9a688f405bd2052407
AffanRidha <affan.ridha02@gmail.com>
2022-01-25 01:05:40 +0700

    finalprojek

---
+++ NewPOS/LoginForm.Designer.cs
@@ -0,0 +1,30 @@
+﻿namespace NewPOS
+{
+    partial class LoginForm
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        private System.Windows.Forms.Label lbluserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.LinkLabel llblRegister;
+        private System.Windows.Forms.Label lblMessageLogin;
+        private System.Windows.Forms.Label label1;
+    }
+}
